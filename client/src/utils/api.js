ba53915fab5d7dfe58d55f20e671ebb0374d160a
AishRH <115287669+AishRH@users.noreply.github.com>
2025-12-18 14:43:04 +0530

    Refactor axios import and enhance baseURL handling

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,19 +1,18 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
 });
 
-api.interceptors.request.use(
-  (config) => {
-    const user = localStorage.getItem("user");
+api.interceptors.request.use((config) => {
+    const user = localStorage.getItem('user');
     if (user) {
-      const { token } = JSON.parse(user);
-      config.headers.Authorization = `Bearer ${token}`;
+        const { token } = JSON.parse(user);
+        config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
-  },
-  (error) => Promise.reject(error)
-);
+}, (error) => {
+    return Promise.reject(error);
+});
 
 export default api;
